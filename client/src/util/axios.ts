d4b045ae5de401dc54f883745c3d2ae6260f623f
Balasubramani K <sonofkrishna.k@gmail.com>
2023-09-18 18:07:59 +0530

    changed live url

--- client/src/util/axios.ts
+++ client/src/util/axios.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-/* export default axios.create({ baseURL: 'https://api.balasubramani-k.in' }); */
-export default axios.create({ baseURL: 'http://localhost:3001/' });
+export default axios.create({ baseURL: 'https://api.balasubramani-k.in' });
+/* export default axios.create({ baseURL: 'http://localhost:3001/' }); */
